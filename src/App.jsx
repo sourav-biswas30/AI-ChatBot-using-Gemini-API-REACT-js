c242ed7becae01ff37e8d2f61e7ce76a65cf92e6
sourav-biswas30 <souravlaglhsub99@gmail.com>
2024-06-07 23:15:44 +0600

    Added all the files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+import axios from "axios";
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer]= useState("");
+
+  async function genAns(){
+    setAnswer("...Your answer is being generated. Please bear with me.");
+    const response= await axios({
+    url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyARx4arz3rtLz__dMpaEuhADB0wiDMbR8w",
+    method:"post",
+    data: {
+      contents:[
+        {parts:[{text:question}]},
+      ],
+    },
+  });
+  setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+  }
+
+  return (
+    <>
+      <h1>AI Chat App</h1>
+      <textarea value={question} onChange={(e)=>setQuestion(e.target.value)} cols="30" row="10"></textarea>
+      <button onClick={genAns}>Generate Response</button>
+      <p>{answer}</p>
+    </>
+  )
+}
+
+export default App
